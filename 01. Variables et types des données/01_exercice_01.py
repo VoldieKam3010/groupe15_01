f085800a304487c43c7d1c707696ab75ee03eb50
VoldieKam3010 <voldiekamuabu@gmail.com>
2025-07-15 14:37:18 +0100

    Add files via upload

--- 01. Variables et types des données/01_exercice_01.py
+++ 01. Variables et types des données/01_exercice_01.py
@@ -1,1 +1,15 @@
-ok
+#Demande d'informations à l'utilisateur
+prenom = input("Entrez votre prenom :")
+age = int(input("Entrez votre âge :"))
+ville = input("Entrez le nom de votre ville :")
+metier = input("Entrez votre metier :")
+
+#Approximation des jours vécus
+jours_vécus = age*365
+
+#Affichage formaté
+print("\n=== PROFIL UTILISATEUR ===")
+print(f"Prénom : {prenom}")
+print(f"Age : {age} ans ({jours_vécus} jours vécus environ)")
+print(f"Ville : {ville}")
+print(f"Metier : {metier}")
